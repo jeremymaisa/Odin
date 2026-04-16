5b89dcbdab48ba5e3bfb258d45bc1c9653d95786
jeremy <maisajeremy331@gmail.com>
2026-04-16 20:01:20 +0800

    rock, paper and scissors

--- rock-paper-scissor/script.js
+++ rock-paper-scissor/script.js
@@ -1,4 +1,4 @@
-const hand = ['rock', 'paper', 'scissors'];
+const hand = ['rock', 'paper', 'scissor'];
 let humanScore = 0;
 let computerScore = 0;
 
@@ -7,18 +7,17 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-  let choice = prompt('rock, paper or scissors');
+  let choice = prompt('rock, paper or scissor');
   return choice.toLowerCase();
 }
 
-
 function playRound(humanChoice, computerChoice) {
   if (humanChoice === computerChoice) {
-    return "It's a tie!";
+    return `It's a tie!`;
   } else if (hand.indexOf(humanChoice) === hand.indexOf(computerChoice) - 1 ||
              hand.indexOf(humanChoice) === hand.indexOf(computerChoice) + 2) {
     computerScore++;
-    return `You lose ${computerChoice} beats ${humanChoice}`;
+    return `You lose ${computerChoice} beats ${humanChoice}`
   } else {
     humanScore++;
     return `You win ${humanChoice} beats ${computerChoice}`;
@@ -30,15 +29,17 @@ function playGame() {
     let humanSelection = getHumanChoice();
     let computerSelection = getComputerChoice();
 
-    console.log('Round: ' + i);
+    console.log(`Round: ${i}`);
     console.log(playRound(humanSelection, computerSelection));
     console.log(`Score: human ${humanScore} - ${computerScore} computer`);
+    console.log("--------------------------");
+    
   }
 
   if (humanScore > computerScore) {
-    return 'You win the game!';
+    return 'You win!';
   } else if (humanScore < computerScore) {
-    return 'You lose the game!';
+    return "You lose!";
   } else {
     return `It's a tie!`;
   }
