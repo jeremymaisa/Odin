394c7d3a2586e0920b15840bd80b0d5d5f413daf
jeremy <maisajeremy331@gmail.com>
2026-05-02 16:34:59 +0800

    javascript project etch - a  - sketch i learn display flex and DOM manipulation and some mouse evets here

---
+++ etch-a-sketch/sketch.js
@@ -0,0 +1,43 @@
+const container = document.querySelector('.grid-container');
+const gridBtn = document.querySelector('#newGrid');
+const reset = document.querySelector('#reset');
+
+gridBtn.addEventListener('click', () => {
+  let newGridSize = prompt('Enter a number of squares per side (max: 100):');
+
+  newGridSize = parseInt(newGridSize);
+
+  if (newGridSize > 0 && newGridSize <= 100) {
+    container.innerHTML = '';
+
+    const newSquare = 640 / newGridSize;
+
+    for (let i = 0; i < newGridSize * newGridSize; i++) {
+      const square = document.createElement('div');
+
+      square.classList.add('square');
+      square.style.width = `${newSquare}px`;
+      square.style.height = `${newSquare}px`;
+
+      square.addEventListener('mouseenter', () => {
+        const r = Math.floor(Math.random() * 256);
+        const g = Math.floor(Math.random() * 256);
+        const b = Math.floor(Math.random() * 256);
+
+        square.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+      });
+    
+      container.appendChild(square);
+    }
+  } else {
+    alert('Plase enter a valid number between 1 to 100.');
+  }
+});
+
+reset.addEventListener('click', () => {
+  const resetBtn = confirm('Are you sure to delete this grid?');
+
+  if (resetBtn) {
+    container.innerHTML = '';
+  }
+})
