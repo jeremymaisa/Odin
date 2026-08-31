b42f2d7685e88d7d49d71fcc7848872654053c98
jeremy <maisajeremy331@gmail.com>
2026-08-31 21:58:39 +0800

    add styling to the card container of a book

--- intermediate_js/library_project/library.js
+++ intermediate_js/library_project/library.js
@@ -14,9 +14,9 @@ function addBookToLibrary(title, author, date) {
   myLibrary.unshift(myBook);
 }
 
-addBookToLibrary("Example1", "Example1", "9-1-2023");
-addBookToLibrary("Example2", "Example2", "10-2-2024");
-addBookToLibrary("Example3", "Example3", "11-3-2025");
+addBookToLibrary("Title1", "Author1", "9-1-2023");
+addBookToLibrary("Title2", "Author2", "10-2-2024");
+addBookToLibrary("Title3", "Author3", "11-3-2025");
 
 function displayBook() {
   const container = document.querySelector('.container');
@@ -28,11 +28,13 @@ function displayBook() {
     card.dataset.id = this.id;
 
     card.innerHTML = `
-      <p>Title: ${book.title}</p>
-      <p>Author: ${book.date}</p>
-      <p>Date: ${book.date}</p>
-      <button class="button-toggle-read">${this.read ? 'read' : 'not read'}</button>
-      <button class="button-remove"><i class="fa fa-trash"></i></button>
+      <p><strong>Title:</strong> ${book.title}</p>
+      <p><strong>Author:</strong> ${book.author}</p>
+      <p><strong>Date:</strong> ${book.date}</p>
+      <div class="btn">
+       <button class="button-toggle-read">${this.read ? 'read' : 'not read'}</button>
+      <button class="button-remove"><i class="fa fa-trash"></i></button></div>
+     
     `
     container.appendChild(card);
   }
